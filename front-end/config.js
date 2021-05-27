7f7a0766b182eeaf612cdfd15b6b794f00d2baa7
helio.dev <helio.dev@protonmail.com>
2021-05-27 14:25:34 +0200

    creation du javascript

--- front-end/config.js
+++ front-end/config.js
@@ -26,6 +26,34 @@ main(
 )
 */
 
+/* 
+main(
+    async function main (){
+        const articles = await getArticles()
+        for (let i = O;i < articles.length; i++){
+            const article = articles[i];
+            displayArticle(articles)
+        }
+    }
+
+    function getArticles(){
+        return fetch("http://localhost:3000/api/teddies")
+            .then(function(httpBodyResponse){
+                return httpBodyResponse()
+            })
+            .then(function(articles){
+            })
+            .catch(function(error){
+                alert(error)
+            })
+    }
+
+    function displayArticles(){
+        document.getElementById("main").innerHTML += '
+        <div class="article__container"></div>'
+)
+*/
+
 fetch("http://localhost:3000/api/teddies")
         .then ((res)=>res.json()
         .then ((resultat)=>{
@@ -39,5 +67,23 @@ fetch("http://localhost:3000/api/teddies")
                 tableauElements[tableauElements.length-1].onclick = () => console.log("hello");
                 tableauElements[tableauElements.length-1].textContent = cur.name;
                 tableauElements[tableauElements.length-2].appendChild(tableauElements[tableauElements.length-1]);
+              	
+                tableauElements.push(document.createElement("p"));
+                tableauElements[tableauElements.length-1].onclick = () => console.log("hello");
+                tableauElements[tableauElements.length-1].textContent = cur.description
+                tableauElements[tableauElements.length-2].appendChild(tableauElements[tableauElements.length-1]);
+              	
+                tableauElements.push(document.createElement("p"));
+                tableauElements[tableauElements.length-1].onclick = () => console.log("hello");
+                tableauElements[tableauElements.length-1].textContent = cur.price
+                tableauElements[tableauElements.length-2].appendChild(tableauElements[tableauElements.length-1]);
+              	
+                tableauElements.push(document.createElement("img"));
+                tableauElements[tableauElements.length-1].onclick = () => console.log("hello");
+                tableauElements[tableauElements.length-1].textContent = cur.imageUrl
+                tableauElements[tableauElements.length-2].appendChild(tableauElements[tableauElements.length-1]);
+                                
+               
+                
             }
         }))
